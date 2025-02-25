dfe49f68eef287b6ce8f251cbf6dda2025ac04d9
Aldhair Lujan Nicolini <aldhajo.1101@gmail.com>
2025-02-25 17:03:13 -0500

    Products

--- src/app/infoproducts/sectionsInfo/info-table/info-table.component.ts
+++ src/app/infoproducts/sectionsInfo/info-table/info-table.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { TabsModule } from 'primeng/tabs';
+import { TableModule } from 'primeng/table';
+
 
 @Component({
   selector: 'app-info-table',
-  imports: [TabsModule],
+  imports: [TabsModule,TableModule],
   templateUrl: './info-table.component.html',
   styleUrl: './info-table.component.css'
 })
