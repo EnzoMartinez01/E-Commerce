1f8075613bbed6d87f8660e56cb2526bcaea3a6a
Enzo Martinez <enzomartinez2412@gmail.com>
2025-02-27 22:36:34 -0500

    Guards fix

--- src/app/core/security/admin.guard.ts
+++ src/app/core/security/admin.guard.ts
@@ -10,7 +10,7 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {}
 
   canActivate(): Observable<boolean> {
-    const token = sessionStorage.getItem('token');
+    const token = sessionStorage.getItem('authToken');
 
     if (!token) {
       this.router.navigate(['/home']).then(r => false);
